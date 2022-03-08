5521da14c8d0a82accfd616c2014daabfb0f7a28
VM <vm@ya.ru>
2022-03-08 17:23:40 -0500

    HW

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        List<Integer> filterList= new ArrayList<>();
+        List<Integer> filterList = new ArrayList<>();
         for (Integer number : intList) {
             if (number > 0 && number % 2 == 0) {
                 filterList.add(number);
